3150225734c7c3c103ae804c43bfccb55b4c5017
Joel Barnes <oelarnes@gmail.com>
2024-09-21 22:52:47 -0400

    script to download files from 17l

--- mdu/cache_17l.py
+++ mdu/cache_17l.py
@@ -1,8 +1,8 @@
-import urllib.request, tomllib, os
+import urllib.request, tomllib, os, gzip, shutil, re
 from functools import lru_cache
 from importlib import resources
 
-DEFAULT_DIR='data/17l-files'
+DEFAULT_DIR = os.path.join('data', '17l-files')
 
 @lru_cache(maxsize=1)
 def load_config():
@@ -10,11 +10,23 @@ def load_config():
     with open(config_path, 'rb') as config:
         return tomllib.load(config)
 
-def download_data_set(set_code, dataset_type, target_dir=DEFAULT_DIR):
+def download_data_set(set_code, dataset_type, force_download=False, target_dir=DEFAULT_DIR):
     config = load_config()
     filepath = config[set_code][dataset_type]
-    targetpath = os.path.join(target_dir, os.path.split(filepath)[1])
+    target_path = os.path.join(target_dir, os.path.split(filepath)[1])
+    unzipped_path = re.split('.gz$', target_path)[0]
+
+    if os.path.isfile(unzipped_path) and not force_download:
+        print(f'file {unzipped_path} already exists, rerun with force_download=True to download')
+        return
     
     if not os.path.isdir(target_dir):
         os.makedirs(target_dir)
-    urllib.request.urlretrieve(filepath, targetpath)
+
+    urllib.request.urlretrieve(filepath, target_path)
+
+    with gzip.open(target_path, 'rb') as f_in:
+        with open(unzipped_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out) 
+    
+    os.remove(target_path)
---
+++ scripts/get_file.py
@@ -0,0 +1,27 @@
+import argparse
+from importlib import resources
+from mdu.cache_17l import load_config, download_data_set
+
+parser = argparse.ArgumentParser(description='Download 17Lands public datasets')
+
+parser.add_argument('sets', metavar='sets', nargs='+',
+                    help='Sets to download. (Download all configured sets if none provided)')
+parser.add_argument('-t', default='draft',
+                    help='File type: draft (default), game, or replay')
+parser.add_argument('-f', default='n', help='force download even if exists')
+
+args = parser.parse_args()
+
+config = load_config()
+
+if len(args.sets) == 0:
+    sets = list(config.keys())
+else:
+    sets = args.sets
+
+file_type = args.t
+force_download = (args.f.lower() == 'y')
+
+for set_code in sets:
+    print(f'Downloading {file_type} file for set {set_code}')
+    download_data_set(set_code, file_type, force_download )
