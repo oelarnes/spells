52a2309b82ef931991cc461bed72917ce4d89ebc
Joel Barnes <oelarnes@gmail.com>
2025-01-11 14:13:07 -0500

    wavg with tests

--- spells/utils.py
+++ spells/utils.py
@@ -30,17 +30,26 @@ def wavg(
         name_list = list(new_names)
 
     assert len(name_list) == len(col_list), f"{len(name_list)} names provided for {len(col_list)} columns"
-    assert len(weight_list) == len(col_list), f"{len(weight_list)} weights provided for {len(col_list)} columns" 
+    assert len(name_list) == len(set(name_list)), "Output names must be unique"
+    assert len(weight_list) == len(col_list) or len(weight_list) == 1, f"{len(weight_list)} weights provided for {len(col_list)} columns" 
+
+    enum_wl = weight_list * int(len(col_list) / len(weight_list))
+    wl_names = [w.meta.output_name() for w in weight_list]
+    assert len(wl_names) == len(set(wl_names)), "Weights must have unique names. Send one weight column or n uniquely named ones"
     
     to_group = df.select(gbs + weight_list + [ 
-        (c * weight_list[i]) for i, c in enumerate(col_list)
+        (c * enum_wl[i]).alias(name_list[i]) for i, c in enumerate(col_list)
     ]) 
 
     grouped = to_group if not gbs else to_group.group_by(gbs)
 
-    return grouped.sum().select(
+    ret_df = grouped.sum().select(
         gbs + 
-        [pl.col(c.meta.output_name()).alias(name_list[i]) for i, c in enumerate(col_list)]
+        wl_names + 
+        [(pl.col(name) / pl.col(enum_wl[i].meta.output_name())) for i, name in enumerate(name_list)]
     )
+   
+    if gbs:
+        ret_df = ret_df.sort(by=gbs)
 
-
+    return ret_df
